2eef801b071d1f5be399c74c321c0762b8161a42
BitGlitched <Flamewarrior558@yahoo.com>
2017-02-06 20:38:56 -0500

    Cleaned up the merge

--- src/App.cpp
+++ src/App.cpp
@@ -33,21 +33,12 @@ void Startup()
 	//First argument provided is the x position, second is the y position, third is the x scale/size, fourth is the y scale/size, and fifth is the texture path
 	// Y = 575 is roughly the bottom of the Interface
 	propertiesPanel = GUIElement(0.0f, 60.0f, 100.0f, 600.0f, TEX_ARROW);
-<<<<<<< HEAD
-  Black_Box = GUIElement (0.0f, 0.0f, 500.0f, 50.0f, Black_Panel);
-              //OPTION = GUIElement (200.0f, 200.0f, 50.0f, 50.0f, Options_Menu);
-  OPTION = GUIElement (18.0f, 18.0f, 25.0f, 25.0f, Options_Menu);
-  Check = GUIElement ( 0.0f, 0.0f, 0.0f, 30.0f, Black_Check);
-  arrow = GUIElement (167.0f, 18.0f, 40.0f, 30.0f, Arrow);
-  Drop = GUIElement (73.0f, 70.0f, 80.0f, 17.0f, DropB);
-=======
    Black_Box = GUIElement (0.0f, 0.0f, 500.0f, 50.0f, Black_Panel);
 	//OPTION = GUIElement (200.0f, 200.0f, 50.0f, 50.0f, Options_Menu);
    OPTION = GUIElement (18.0f, 18.0f, 25.0f, 25.0f, Options_Menu);
    Check = GUIElement ( .0f, 0.0f, .0f, 30.0f, Black_Check);
    arrow = GUIElement (167.0f, 18.0f, 40.0f, 30.0f, Arrow);
    Drop = GUIElement (73.0f, 70.0f, 80.0f, 17.0f, DropB);
->>>>>>> origin/master
 }
 
 //Function updated every frame
@@ -57,26 +48,10 @@ void Update()
 	propertiesPanel.transform.h = WindowSize.y;  //propertiesPanel.transform.y; //- propertiesPanel.transform.x; wth andrew. You're litterally subtracting Zero...
 
    Black_Box.transform.w = WindowSize.x;    //  / 5.0f;
-<<<<<<< HEAD
-   Black_Box.transform.h = WindowSize.y / 9.7f ;
-
-   OPTION.transform.w = 30/*WindowSize.x / 35*/ ;
-   OPTION.transform.h = 30/*WindowSize.y / 21*/ ;
-
-  //arrow.transform.w = WindowSize.x / 17;
-  //arrow.transform.h = WindowSize.y / 25;
-  //arrow.transform.x = WindowSize.x + 30;
-  arrow.transform.x = WindowSize.x / 4.5f ;
-  arrow.transform.y = WindowSize.y / 30 ;
-
-  Drop.transform.x = WindowSize.x / 11.5f;
-  Drop.transform.y = WindowSize.y / 7.4f ;
-=======
    Black_Box.transform.h = WindowSize.y / 9.7f;
 
    OPTION.transform.w = WindowSize.x / 35 ;
    OPTION.transform.h = WindowSize.y / 21 ;
->>>>>>> origin/master
 
    //arrow.transform.w = WindowSize.x / 17;
    //arrow.transform.h = WindowSize.y / 25;
@@ -92,20 +67,12 @@ void Update()
 
 	propertiesPanel.Draw();
    Black_Box.Draw();
-<<<<<<< HEAD
-
-   Check.Draw();
-   OPTION.Draw();
-   arrow.Draw();
-   Drop.Draw();
-=======
 
    Check.Draw();
    OPTION.Draw();
    arrow.Draw();
    Drop.Draw();
 
->>>>>>> origin/master
 	//Checking for clicking a GUIElement example:
 	//if (testElement.CheckHover(Mouse) && Mouse.LeftClickDown)
 	//{
