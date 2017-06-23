bec4050c6eae4ab8a6f8be4cef0deb2b3cb40113
QuasarGNK <zwilliams71@hotmail.com>
2017-06-23 02:21:56 -0400

    Component Buttons
    Added placement preview and buttons for components

--- src/Interface.cpp
+++ src/Interface.cpp
@@ -5,24 +5,48 @@ GUIElement wireToolButton;
 GUIElement propertiesPanel;
 GUIElement pickerPanel;
 GUIElement arrow;
+GUIElement forwardArrow;
 GUIElement dropbownBox;
 GUIElement grayBackground;
 GUIElement checkBoxChecked;
 GUIElement grid;
+GUIElement battery;
+GUIElement wire;
+GUIElement resistor;
+GUIElement capacitor;
+GUIElement integratedCircuit;
+GUIElement currentComponent;
+GUIElement temp;
+
+ToolType toolSelected = NONE;
+int gridLockX;
+int gridLockY;
 
 int tempText;
 TTF_Font* font;
 
 Vector2 shiftViewStart;
+Vector2 shiftViewOffset;
+
+int componentOffsetX;
+int componentOffsetY;
 
 void InitGUI()
 {
+	arrow = GUIElement(-388.0f, 319.0f, 1.0f, 1.0f, TEX_ARROWLEFT);
+	forwardArrow = GUIElement(-356.0f, 319.0f, 1.0f, 1.0f, TEX_ARROWRIGHT);
 	//wireToolButton = GUIElement(
-	propertiesPanel = GUIElement(-522.0f, 0.0f, 1.0f, 1.0f, TEX_PPANEL);
+	propertiesPanel = GUIElement(-522.0f, 0.0f, 1.0f, 1.0f, TEX_PPANEL); //right edge at -404
    pickerPanel = GUIElement (0.0f, 319.0f, 1.0f, 1.0f, TEX_PICKER_PANEL);
    //dropbownBox = GUIElement (18.0f, 18.0f, 0.1f, 0.1f, TEX_DROPDOWN_BOX); //OPTION = GUIElement (200.0f, 200.0f, 50.0f, 50.0f, Options_Menu);
    //checkBoxChecked = GUIElement (100.0f, 100.0f, 0.1f, 0.1f, TEX_CHECKBOX_CHECKED);
-   arrow = GUIElement (-150.0f, 319.0f, 1.0f, 1.0f, TEX_ARROW);
+   battery = GUIElement (-292.0f, 319.0f, 1.0f, 1.0f, TEX_IC);
+   resistor = GUIElement (-228.0f, 319.0f, 1.0f, 1.0f, TEX_IC);
+   capacitor = GUIElement (-164.0f, 319.0f, 1.0f, 1.0f, TEX_IC);
+   integratedCircuit = GUIElement(-100.0f, 319.0f, 1.0f, 1.0f, TEX_IC);
+   wire = GUIElement(-36.0f, 319.0f, 1.0f, 1.0f, TEX_IC);
+   currentComponent = GUIElement();
+   temp = GUIElement (0, 0, 0.10f, 0.10f, TEX_IC);
 	grid = GUIElement (0, 0, 160.0f, 90.0f, TEX_GRID);
 	grid.texture.mode = REPEAT;
 
@@ -32,14 +56,19 @@ void InitGUI()
 
 void MoveView()
 {
-	grid.offset.x += (float)Mouse.velX * 0.00075f;
-	grid.offset.y += (float)Mouse.velY * 0.00075f;
-	printf("Offset: %f, %f\n", grid.offset.x, grid.offset.y);
+	grid.offset.x += (float)Mouse.velX * 0.001f;
+	grid.offset.y += (float)Mouse.velY * 0.001f;
+
+	shiftViewOffset.x += (float)Mouse.velX * 0.001f;
+	shiftViewOffset.y += (float)Mouse.velY * 0.001f;
+
+	//componentOffsetX = grid.offset.x - (int)grid.offset.x;
+	//componentOffsetY = grid.offset.y - (int)grid.offset.y;
 }
 
 void UpdateGUI()
 {
-	if (Mouse.MiddleClickPressed)
+	/*if (Mouse.MiddleClickPressed)
 	{
 		if (Mouse.MiddleClickDown)
 		{
@@ -47,11 +76,49 @@ void UpdateGUI()
 		}
 
 		MoveView();
-	}
+	}*/
 
 	Draw2D(GUIShader, Vector2(), Vector2(1, 1), tempText);
-	propertiesPanel.CheckHover();
-	pickerPanel.CheckHover();
+	bool propertiesHover = propertiesPanel.CheckHover();
+	bool pickerHover = pickerPanel.CheckHover();
+
+
+	if (Mouse.LeftClickDown)
+	{
+		if (battery.CheckHover())
+		{
+			toolSelected = BATTERY;
+		}
+		if (wire.CheckHover())
+		{
+			toolSelected = WIRE;
+		}
+		if (resistor.CheckHover())
+		{
+			toolSelected = RESISTOR;
+		}
+		if (capacitor.CheckHover())
+		{
+			toolSelected = CAPACITOR;
+		}
+		if (integratedCircuit.CheckHover())
+		{
+			toolSelected = IC;
+		}
+	}
+
+	if (toolSelected != NONE)
+	{
+		if(!pickerHover && !propertiesHover)
+		{
+			currentComponent.ChangeImage(TEX_IC);
+			currentComponent.Draw();
+			gridLockX = Mouse.x - (Mouse.x % 16);//(shiftViewOffset.x - componentOffsetX);
+			gridLockY = Mouse.y - (Mouse.y % 16);//(shiftViewOffset.y - componentOffsetY);
+			currentComponent.transform.x = gridLockX;
+			currentComponent.transform.y = gridLockY;
+		}
+	}
 }
 
 void DrawGUI()
@@ -60,4 +127,11 @@ void DrawGUI()
 	propertiesPanel.Draw();
 	pickerPanel.Draw();
 	arrow.Draw();
+	battery.Draw();
+	resistor.Draw();
+	capacitor.Draw();
+	forwardArrow.Draw();
+	wire.Draw();
+	integratedCircuit.Draw();
+	temp.Draw();
 }
--- src/headers/GUI.h
+++ src/headers/GUI.h
@@ -3,7 +3,8 @@
 
 #define PROTOTYPING "data/Prototyping.png"
 #define TEX_DROPDOWN_MENU "data/DropdownMenu.png"
-#define TEX_ARROW "data/ARROW.png"
+#define TEX_ARROWLEFT "data/ARROW-Left.png"
+#define TEX_ARROWRIGHT "data/ARROW-Right.png"
 #define TEX_DROPDOWN_BG "data/DropdownBackground.png"
 #define TEX_GRAY_BG "data/GrayBackground.png"
 #define TEX_PICKER_PANEL "data/CircuitPickerPanel.png"
@@ -11,9 +12,19 @@
 #define TEX_DROPDOWN_BOX "data/DropdownBox.png"
 #define TEX_PPANEL "data/PropertiesPanel.png"
 #define TEX_GRID "data/Grid.png"
-#define TEX_IC "data/IC.png"
+#define TEX_IC "data/IC-1.png"
 
 #include "types/GUIElement.h"
+
+enum ToolType
+{
+    WIRE,
+    BATTERY,
+    RESISTOR,
+    CAPACITOR,
+    IC,
+    NONE
+};
 
 void InitGUI();
 void UpdateGUI();
